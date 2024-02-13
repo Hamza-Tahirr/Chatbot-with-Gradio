d5e6e5870860cd3901924b23d56c567aef3df80d
Hamza Tahir <90556877+Hamza-Tahirr@users.noreply.github.com>
2024-02-13 07:56:13 +0000

    initial

---
+++ main.py
@@ -0,0 +1,46 @@
+import gradio as gr
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+
+from langchain.document_loaders import PyPDFLoader
+import os
+
+import fitz
+from PIL import Image
+
+with gr.Blocks() as demo:
+    # Create a Gradio block
+
+    with gr.Column():
+        with gr.Row():
+            with gr.Column(scale=0.8):
+                api_key = gr.Textbox(
+                    placeholder='Enter OpenAI API key',
+                    show_label=False,
+                    interactive=True
+                ).style(container=False)
+            with gr.Column(scale=0.2):
+                change_api_key = gr.Button('Change Key')
+
+        with gr.Row():
+            chatbot = gr.Chatbot(value=[], elem_id='chatbot').style(height=650)
+            show_img = gr.Image(label='Upload PDF', tool='select').style(height=680)
+
+    with gr.Row():
+        with gr.Column(scale=0.70):
+            txt = gr.Textbox(
+                show_label=False,
+                placeholder="Enter text and press enter"
+            ).style(container=False)
+
+        with gr.Column(scale=0.15):
+            submit_btn = gr.Button('Submit')
+
+        with gr.Column(scale=0.15):
+            btn = gr.UploadButton("📁 Upload a PDF", file_types=[".pdf"]).style()
+
+
